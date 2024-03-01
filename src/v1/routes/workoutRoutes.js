0d13e644b8918d79b65cb958bcd4738eb7b1b5b0
jddeltoro <jdiazdeltoro@gmail.com>
2024-02-29 23:23:34 -0500

    add json api squemas

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,8 +1,48 @@
 const express = require('express');
+const apicache = require('apicache');
 const workoutController = require('../../controllers/workoutController');
 const router = express.Router();
+const cache = apicache.middleware;
 
-router.get('/', workoutController.getAllWorkouts);
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items: 
+ *                    $ref: '#/components/schemas/Workout'
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
+router.get('/', cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get('/:id', workoutController.getWorkoutById);
 
